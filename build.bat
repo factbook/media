4716594a817008fb960a4505dedad1c63c819ced
Gerald Bauer <gerald.bauer@gmail.com>
2021-02-02 20:14:02 +0100

    recycle

--- build.bat
+++
@@ -1,4 +0,0 @@
-csc /nologo test\test_client.cs
-csc /nologo test\test_client_ii.cs
-csc /nologo test\test_client_iii.cs
-csc /nologo proxy.cs
--- setenv.bat
+++
@@ -1,1 +0,0 @@
-set PATH=C:\Windows\Microsoft.NET\Framework\v3.5;%PATH%
